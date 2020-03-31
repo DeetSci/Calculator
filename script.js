a7996eb2da51e4b1455b0d082b4f0ae9139456e7
DeetSci <47761940+DeetSci@users.noreply.github.com>
2020-03-31 17:41:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,183 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+  }
+
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+  }
+
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === '') 
+        return
+    if (this.previousOperand !== '') {
+        this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
+  }
+
+  compute() {
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) {
+      return
+    }
+    switch (this.operation) {
+      case '/':
+        computation = prev / current
+        if (current == 0) {
+          alert("Division by Zero")
+        } else if (!Number.isSafeInteger(computation)) {
+          alert("Integer Overflow")
+        }
+        break
+      case '*':
+        computation = prev * current
+        if (!Number.isSafeInteger(computation)) {
+          alert("Integer Overflow")
+        }
+        break
+      case '+':
+        computation = prev + current
+        if (!Number.isSafeInteger(computation)) {
+          alert("Integer Overflow")
+        }
+        break
+      case '-':
+        computation = prev - current
+        if (!Number.isSafeInteger(computation)) {
+          alert("Integer Overflow")
+        }
+        break
+      case 'sqrt':
+        computation = Math.sqrt(prev)
+        if (!Number.isSafeInteger(computation)) {
+          alert("Integer Overflow")
+        }
+        break
+      case 'sin':
+        computation = Math.sin(prev)
+        if (!Number.isSafeInteger(computation)) {
+          alert("Integer Overflow")
+        }
+        break
+      case 'cos':
+        computation = Math.cos(prev)
+        if (!Number.isSafeInteger(computation)) {
+          alert("Integer Overflow")
+        }
+        break
+      case 'tan':
+        computation = Math.tan(prev)
+        if (!Number.isSafeInteger(computation)) {
+          alert("Integer Overflow")
+        }
+        break
+      default:
+        return
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
+  }
+
+  scientificCompute() {
+
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
+  }
+
+  updateDisplay() {
+    this.currentOperandTextElement.innerText =
+      this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText =
+      `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    } else {
+      this.previousOperandTextElement.innerText = ''
+    }
+  }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const scientificButton = document.querySelectorAll('[data-scientific]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    document.getElementsByClassName("numbers").translation
+    calculator.updateDisplay()
+    if (calculator > 5) {
+      alert("Integer Overflow")
+    }
+  })
+})
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+scientificButton.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+    calculator.appendNumber(0)
+  })
+})
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+  calculator.delete()
+  calculator.updateDisplay()
+})
